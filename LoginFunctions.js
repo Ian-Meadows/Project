122e710ffa8d893f447db9581979103e8fc5e10e
Ian-Meadows <iame5815@colorado.edu>
2018-10-15 18:02:34 -0600

    Added some basic js functionality

---
+++ LoginFunctions.js
@@ -0,0 +1,11 @@
+window.onload = function(){ 
+    // your code
+    document.getElementById("LoginButton").onclick = function(){
+    	var username = document.getElementById("UsernameInput").value;
+    	var password = document.getElementById("PasswordInput").value;
+		if(username == "user" && password == "password"){
+			document.location.href = "Profile Page.html";
+		}
+	};  
+};
+
