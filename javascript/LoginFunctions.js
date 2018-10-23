fcd69e39e7e9c02d1b77067d15e3fa2a6a21f847
Ian-Meadows <iame5815@colorado.edu>
2018-10-22 18:41:01 -0600

    Added more cookie functions for main page and login page.

--- javascript/LoginFunctions.js
+++ javascript/LoginFunctions.js
@@ -1,4 +1,7 @@
 window.onload = function(){ 
+	if(document.cookie == "true"){
+		document.location = 'Profile Page.html';
+	}
     // your code
     document.getElementById("LoginButton").onclick = function(){
     	
@@ -6,9 +9,9 @@ window.onload = function(){
     	var username = document.getElementById("UsernameInput").value;
     	var password = document.getElementById("PasswordInput").value;
 		if(username == "user" && password == "password"){
+			document.cookie="true";
 			document.location = 'Profile Page.html';
 		}
 		return false;	
 	};  
 };
-
