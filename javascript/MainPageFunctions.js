6e7a013f38448ec83bd00a80c9e5829e0261f223
Ian-Meadows <iame5815@colorado.edu>
2018-10-22 18:12:14 -0600

    Added main page javascript functionality. Now with 100% more cookies

---
+++ javascript/MainPageFunctions.js
@@ -0,0 +1,24 @@
+window.onload = function(){ 
+	
+	var p = document.getElementById("ButtonText");
+	var cookie = document.cookie;
+	if(cookie == "true"){
+		p.innerHTML = "Profile";
+	}
+	else{
+		p.innerHTML = "Login";
+	}
+
+	document.getElementById("LoginButton").onclick = function(){
+		if(cookie == "true"){
+			document.location = 'Profile Page.html';
+		}
+		else{
+			document.location = 'Login Page.html';
+		}
+		return false;
+	};
+
+
+
+};
