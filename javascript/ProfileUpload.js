53e2d9b81acb8e8243a223f55eb6b99e4b006aea
Siyu Yao <43591415+ysyyuune@users.noreply.github.com>
2018-11-06 08:23:14 +0800

    update

--- javascript/ProfileUpload.js
+++ javascript/ProfileUpload.js
@@ -1,21 +1,10 @@
-<script>
-function handleFiles(obj,id) {
-    file = document.getElementById("icon");
-    var files = obj.files;
-    img = new Image();
-    if(window.URL){
-        //File API
-        img.src = window.URL.createObjectURL(files[0]);
-        img.onload = function(e) {
-        window.URL.revokeObjectURL(this.src);
+$('#chooseImage').on('change',function(){
+        var filePath = $(this).val(),
+            fileFormat = filePath.substring(filePath.lastIndexOf(".")).toLowerCase();
+        if( !fileFormat.match(/.png|.jpg|.jpeg/) ) {
+            error_prompt_alert('file format：png/jpg/jpeg');
+            return;  
         }
-    }
-    file.src=img.src;
-    //上传文件
-        var fd = new FormData(),
-        xhr = new XMLHttpRequest();
-        fd.append('headimg', files[0]);
-        xhr.open('post', 'user.php?act=act_edit_img');
-        xhr.send(fd);
-}
-</script>
+        $('#cropedBigImg').attr('src',filePath);
+        $('#loding').show();
+});
