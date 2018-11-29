f4544edbfeda59c25b6319fedfecb7a5813f6e44
jajo8801 <jajo8801@colorado.edu>
2018-11-29 14:17:45 -0700

    fixed res.sendfile error

--- PageHandler.js
+++ PageHandler.js
@@ -3,37 +3,37 @@ var app = express();
 
 
 app.get('/GroupJoinPage', function(req, res){
-    res.sendfile('Public/html/GroupJoinPage.html');
+    res.sendFile(__dirname + '/Public/html/GroupJoinPage.html');
 });
 
 app.get('/GroupCreationPage', function(req, res){
-    res.sendfile('Public/html/GroupCreationPage.html');
+    res.sendFile(__dirname + '/Public/html/GroupCreationPage.html');
 });
 
 app.get('/GroupPage', function(req, res){
-    res.sendfile('Public/html/GroupPage.html');
+    res.sendFile(__dirname + '/Public/html/GroupPage.html');
 });
 
 app.get('/ProfilePage', function(req, res){
-    res.sendfile('Public/html/ProfilePage.html');
+    res.sendFile(__dirname + '/Public/html/ProfilePage.html');
 });
 
 app.get('/AccountCreationPage', function(req, res){
-    res.sendfile('Public/html/AccountCreationPage.html');
+    res.sendFile(__dirname + '/Public/html/AccountCreationPage.html');
 });
 
 app.get('/AfterLoginPage', function(req, res){
-    res.sendfile('Public/html/AfterLoginPage.html');
+    res.sendFile(__dirname + '/Public/html/AfterLoginPage.html');
 });
 
 app.get('/LoginPage', function(req, res){
-    res.sendfile('Public/html/LoginPage.html');
+    res.sendFile(__dirname + '/Public/html/LoginPage.html');
 });
 
 app.get('/', function(req, res){
     //res.writeHeader(200, {"Content-Type":"text/html"});
-    res.sendfile('Public/html/LandingPage.html');
-    //res.sendfile('javascript/ClientSide/ConnectionTest.js');
+    res.sendFile(__dirname + '/Public/html/LandingPage.html');
+    //res.sendFile('javascript/ClientSide/ConnectionTest.js');
     //res.end();
 });
 
