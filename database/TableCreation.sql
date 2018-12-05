99f014a2e4d6ea00eaee69bafdaaac4dd8fa576b
Ian-Meadows <iame5815@colorado.edu>
2018-12-04 21:45:58 -0700

    Modified database file

--- database/TableCreation.sql
+++ database/TableCreation.sql
@@ -8,50 +8,22 @@ CREATE TABLE IF NOT EXISTS Users(
 );
 
 
-
-CREATE TABLE IF NOT EXISTS Game(
-	ID int NOT NULL UNIQUE,
-	Home varchar(55),
-	HomeScore int,
-	Visitor varchar(55),
-	VisitorScore int,
-	Description varchar(255),
-	StartDate DATE,
-	Status varchar(20),
-	PRIMARY KEY(ID)
-
-);
-
 CREATE TABLE IF NOT EXISTS GroupTable(
 
 	ID serial NOT NULL,
 	Name varchar(55) NOT NULL,
 	Description varchar(255),
 	Password varchar(255),
-	Max_Members int,
+	GroupOwner varchar(32) NOT NULL,
+	Poll varchar(255) NOT NULL,
+	Option1 varchar(32) NOT NULL,
+	Option2 varchar(32) NOT NULL,
+	betsOn1 int NOT NULL,
+	betsOn2 int NOT NULL,
+	pollStatus varchar(32) NOT NULL,
+
 	PRIMARY KEY(ID)
 );
-
-CREATE TABLE IF NOT EXISTS UserGame(
-	UserID int PRIMARY KEY,
-	GameID int,
-	foreign key(UserID)references Users(ID),
-	foreign key(GameID)references Game(ID)
-
-);
-
-CREATE TABLE IF NOT EXISTS Bets(
-	UserID int PRIMARY KEY,
-	GameID int,
-	GroupID int,
-
-	foreign key(UserID) references Users(ID),
-	foreign key(GameID) references Game(ID),
-	Bet int,
-	foreign key(GroupID) references GroupTable(ID)
-);
-
-
 
 CREATE TABLE IF NOT EXISTS UserGroup(
 	UserID int,
@@ -62,11 +34,3 @@ CREATE TABLE IF NOT EXISTS UserGroup(
 
 );
 
-CREATE TABLE IF NOT EXISTS GroupGame(
-	GroupID int PRIMARY KEY,
-	GameID int,
-
-	foreign key(GroupID) references GroupTable(ID),
-	foreign key(GameID) references Game(ID)
-
-);
